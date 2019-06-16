adcfb62e945c2b7f70bf1b5c696142bb3258b019
Shanjida6 <shanjida.a@gmail.com>
2019-06-16 18:42:50 -0400

    merging db and auth branches

--- server/models/user.js
+++
@@ -1,6 +0,0 @@
-const Sequelize = require('sequelize'); 
-const bcrypt = require('bcrypt'); 
-
-//creating a sequealize instance with our db 
-const sequealize = new Sequelize('postgres://postgres@localhost:5432/auth-system')
-
