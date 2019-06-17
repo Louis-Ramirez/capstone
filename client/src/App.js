cee79a8107eec12d7e09d2a45c12f9e79a893f79
Shu <shu.liang89@gmail.com>
2019-06-17 13:44:48 -0400

    setup frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Post from "./post";
 
 function App() {
   return (
--- client/src/post.js
+++ client/src/post.js
@@ -1,1 +1,17 @@
 //post.js
+
+import React, {Component} from 'react';
+
+class Post extends Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(
+    );
+  }
+}
+
+
+export default Post
