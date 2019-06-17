46d77c20402646a64ddb04e31c2347863ae999ba
Erica Elliston <erica.elliston06@myhunter.cuny.edu>
2019-06-17 11:38:49 -0400

    added functionalities to home page

--- client/src/home.js
+++ client/src/home.js
@@ -2,9 +2,47 @@
 
 import React from 'react';
 import './home.css'
+import CreatePostForm from './createPostForm';
 
 
 class Home extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            username: '',
+            imageUrl: '',
+            createPost: false,
+            sideBar: false
+        }
+    }
+    renderForm = () =>{
+        this.setState({createPost: true});
+    }
+    closeForm = () => {
+        this.setState({createPost: false});
+    }
+    showSidebar = () => {
+          this.setState({sideBar: true})
+    }
+
+    hideSideBar = () =>{
+        this.setState({sideBar: false});
+    }
+
+    sideBar = () => {
+        return (
+        <div className="three_sidebar" onClick={this.hideSideBar}>
+        <div className="three_credentials three_sideSub">
+            <img src="https://data.whicdn.com/images/320568210/large.jpg" alt="Cartoon Girl"/>
+            <h3> Cartoon Girl</h3>
+        </div>
+        <div className="three_createPostBtn three_sideSub">
+                <button id="three_createPost" onClick={this.renderForm}>Create Post</button>
+        </div>
+    </div>
+    )
+    }
+
     render(){
         const questions = ["Why is the sky blue ?", "Can we teach dogs to talk ?", "How many jelly beans can fit into the pacific ocean ?", "What is the best Island to vacation on ?", " Who was the greatest man alive ?"];
         const postList = questions.map(post => {
@@ -19,20 +57,22 @@ class Home extends React.Component{
                     <header>
                         <h1>Welcome</h1>
                     </header>
+                    {/*<CreatePost show={this.state.createPost} handleClose={this.closeForm} /> */}
                     <div className="three_list">
                         <h3>Recent: </h3>
                         {postList}
                     </div>
                 </div>
-                <div className="three_sidebar">
-                    <div className="three_credentials three_sideSub">
-                        <img src="https://data.whicdn.com/images/320568210/large.jpg" alt="Cartoon Girl"/>
-                        <h3> Cartoon Girl</h3>
-                    </div>
-                    <div className="three_createPostBtn three_sideSub">
-                            <button id="three_createPost">Create Post</button>
-                    </div>
-                </div>
+                {this.state.sideBar ? <div className="three_sidebar" onClick={this.hideSideBar}>
+        <div className="three_credentials three_sideSub">
+            <img src="https://data.whicdn.com/images/320568210/large.jpg" alt="Cartoon Girl"/>
+            <h3> Cartoon Girl</h3>
+        </div>
+        <div className="three_createPostBtn three_sideSub">
+                <button id="three_createPost" onClick={this.renderForm}>Create Post</button>
+        </div>
+    </div> : <button onClick={this.showSidebar}>Show Panel</button>}
+
             </div>
         )
     }
