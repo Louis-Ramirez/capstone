03935a13f66a2958f9abd319967837a8d9bb2f9f
Shu <shu.liang89@gmail.com>
2019-06-17 11:06:18 -0400

    Copied new backend into serverNew folder

---
+++ serverNew/server/controllers/comment.js
@@ -0,0 +1,95 @@
+import model from '../models';
+
+const { Comment } = model;
+
+class Comments {
+  // create method for posts
+  static createComment(req, res) {
+    console.log("activated======");
+    const { body } = req["query"]
+    const { userId, postId } = req.params
+    return Comment
+      .create({
+        body,
+        userId,
+        postId
+      })
+      .then(commentData => res.status(201).send({
+        message: `Comment created!`,
+        commentData
+      }))
+  }
+
+  static list(req, response) {
+    return Comment
+      .findAll()
+      .then(c =>
+        response.status(200).send(c)
+      );
+  }
+
+  static listPostComments(req,res){
+    return Comment
+      .findAll({
+        where: {postId: req.params.postId }
+      })
+      .then((c) =>
+        res.status(200).send(c)
+      )
+  }
+
+  static modifyComment(request, response){
+    const {body} = request["query"]
+    return Comment
+      .findOne({
+        where: {id: request.params.commentId}
+      })
+      .then((c) => {
+        console.log("results = ", c);
+        c.update({
+          body: body || c.body
+        })
+
+        .then((updatedComment) => {
+          response.status(200).send({
+            message: 'Comment updated successfully',
+            data: {
+              body: body || updatedComment.body
+            }
+          })
+        })
+
+        .catch(error => res.status(400).send(error));
+
+      })
+
+      .catch(error => res.status(400).send(error));
+  }
+
+  static deleteComment(req, res) {
+    return Comment
+      .findOne({
+        where: {id: req.params.commentId}
+      })
+      .then(c => {
+        if(!c){
+          return res.status(400).send({
+            message: 'Comment Not Found'
+          });
+        }
+
+        return c
+          .destroy()
+          .then(() => res.status(200).send({
+            message: 'Post successfully deleted'
+          }))
+          .catch(error => res.status(400).send(error));
+      })
+      .catch(error => res.status(400).send(error))
+  }
+
+
+
+}
+
+export default Comments;
