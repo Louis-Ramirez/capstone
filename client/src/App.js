024c04c235d16b3eaf2a267a41e0fbd89879e5e2
Starasia Wright <starasia.wright@gmail.com>
2019-06-18 16:34:29 -0400

    styling complete, need to build forms

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react';
 import LandingPage from './landing.js';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+// import signup from './signup.js';
+// import login from './login.js';
+// import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
 
 class App extends Component {
   render(){
-  	return(
-  		<Router>
-  			<Route path="/" exact component={Landing}/>
-  			<Route path="/signup" component={signUp}/>
-  			<Route path="/login" component={LogIn}/>
-  		</Router>
-  		)
+  	return(<LandingPage/>)
   }
 }
 export default App;
