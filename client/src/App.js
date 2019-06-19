8a288438c23597dd38e4b611e4aee5ff26a7716c
Louis Ramirez <Louis.Ramirez60@myhunter.cuny.edu>
2019-06-19 14:26:28 -0400

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-import LandingPage from './landing.js';
-import CreatePost from './createPostForm.js';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import './App.css';
-=======
 import LandingPage from './components/landing.js';
 import signup from './components/signUp.js';
 import login from './components/login.js';
 import Home from './components/home.js';
+import CreatePost from './components/createPostForm.js';
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import './styles/App.css';
->>>>>>> 70df8cad007cdcdd2bd3da3d6b80feae42bfcba7
 
 class App extends Component {
   render(){
@@ -25,13 +19,9 @@ class App extends Component {
   			</Switch>
   		</Router>
   		)
-<<<<<<< HEAD
     // return(<LandingPage/>)
     //make a router
 	//return(<CreatePost>)
 }
-=======
-  }
->>>>>>> 70df8cad007cdcdd2bd3da3d6b80feae42bfcba7
 }
 export default App;
