fde51b0292a1a72c92612bf8b332b83be1507114
Erica Elliston <erica.elliston06@myhunter.cuny.edu>
2019-06-20 08:09:45 -0400

    sign up working

--- serverNew/server/controllers/user.js
+++ serverNew/server/controllers/user.js
@@ -11,7 +11,7 @@ class Users {
   // signUp method for Users class
   static signUp(req, res) {
     //password encryption
-    bcrypt.hash(req.query.password, 12, (error, hash) =>{
+    bcrypt.hash(req.body.password, 12, (error, hash) =>{
       if(error){
         return res.status(500).json({
           message: error
@@ -19,35 +19,31 @@ class Users {
       } else{
         return User
         .create({
-          username : req.query.username,
-          email: req.query.email,
-          password: hash,
-          imageUrl: req.query.imageUrl
+          username : req.body.username,
+          email: req.body.email,
+          password: hash
         })
-        .then(userData => {
-          res.status(201).send({
+        .then(res.status(201).send({
           success: true,
-          message: 'User successfully created',
-          userData
-        })})
-        .catch(error  => res.status(500).json({
+          message: 'User successfully created'
+        }))
+        .catch(error  => {
+          res.status(500).json({
             success: false,
             message: error.errors[0].message
-        }))
+        })})
       }
     })
   }
 
   //sign in authenitcation method
   static signIn(req, res){
-    console.log('THE EMAIL IS ============>>>>>>', req)
     //check if email is a valid email
       User.findOne({
          where: { email: req.body.email}
 
       })
       .then(user => {
-        console.log("User ===============",user)
             if (user ==null){
               return res.status(401).json({
                 success: false,
@@ -55,9 +51,7 @@ class Users {
               })
             }
             bcrypt.compare(req.body.password, user.password, (error, result)=>{
-              console.log("result ============>", result);
               if(error){
-                console.log("error ============>", error);
                return res.status(401).json({
                  success: false,
                  message: "Authentication Failed: user not found"
@@ -72,7 +66,7 @@ class Users {
                   success: true,
                   message: "User has been signed in!",
                   token: 'Bearer'+' ' + token,
-                  user: {username: user.username, imageUrl: user.imageUrl, email: user.email}
+                  user: {username: user.username, imageUrl: user.imageUrl, email: user.email, id: user.id}
                 })
               }
               else{
