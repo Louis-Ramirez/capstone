5bb2dfba7d26b175e33dc527b96af9c79b0dbc32
Erica Elliston <erica.elliston06@myhunter.cuny.edu>
2019-06-20 14:04:25 -0400

    User credentials sent to store

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -12,12 +12,31 @@ class Home extends React.Component{
     constructor(props){
         super(props);
         this.state={
-            username: '',
-            imageUrl: '',
+            id: " ",
+            username: " ",
+            imageUrl: " ",
+            email: " ",
             createPost: false,
             sideBar: false
         }
     }
+
+    componentDidMount () {
+        console.log(this.props);
+        if(this.props.history.location.state === undefined){
+            console.log("user not sent");
+        }
+        else {
+             let user = this.props.history.location.state.user;
+             console.log(user);
+            this.setState({...this.state, 
+            id: user.id,
+            username: user.username,
+            imageUrl: user.imageUrl,
+            email: user.email
+        });
+    }
+}
     
     renderForm = () =>{
         this.setState({createPost: true});
@@ -33,11 +52,12 @@ class Home extends React.Component{
         this.setState({sideBar: false});
     }
 
-    sideBarView = (
+    sideBarView = () => (
         <div className="three_sidebar" onMouseLeave={this.hideSideBar} >
         <div className="three_credentials three_sideSub">
-            <img src="https://data.whicdn.com/images/320568210/large.jpg" alt="Cartoon Girl"/>
-            <h3> Cartoon Girl</h3>
+            <img src={this.state.imageUrl} alt="User"/>
+            <h3>{this.state.username}</h3>
+            <p>{this.state.email}</p>
         </div>
         <div className="three_createPostBtn three_sideSub">
                 <button id="three_createPost" onClick={this.renderForm}>Create Post</button>
@@ -45,7 +65,8 @@ class Home extends React.Component{
     </div>
     )
 
-    TempForm = (
+
+   /* TempForm = (
         <div  className="three_form" >
         <form  onSubmit={this.closeForm} className="modal-main" >
             <label>Title</label>
@@ -59,10 +80,11 @@ class Home extends React.Component{
             <input type="submit" value="submit"/>
         </form>
         </div>
-    )
+    )*/
     
 
     render(){
+        console.log(this.state);
         const showHide = this.state.createPost ? "three_form display-block" : "three_form  display-none";
         const questions = ["Why is the sky blue ?", "Can we teach dogs to talk ?", "How many jelly beans can fit into the pacific ocean ?", "What is the best Island to vacation on ?", " Who was the greatest man alive ?"];
         const postList = questions.map(post => {
@@ -80,14 +102,14 @@ class Home extends React.Component{
                         <h1>Welcome</h1>
                         <Link to="/signout"><button>sign out</button></Link>
                     </header>
-                    {this.state.createPost ? <div className={showHide} >{this.TempForm}</div>: <div></div>}
+                    {this.state.createPost ? <div className={showHide} ><Input/></div>: <div></div>}
                     {/*<CreatePost show={this.state.createPost} handleClose={this.closeForm} /> */}
                     <div className="three_list">
                         <h3>Recent: </h3>
                         {postList}
                     </div>
                 </div>
-                {this.state.sideBar ? this.sideBarView : <div  id="three_default" onMouseEnter={this.showSidebar}> </div> }
+                {this.state.sideBar ? this.sideBarView() : <div  id="three_default" onMouseEnter={this.showSidebar}> </div> }
 
             </div>
         )
