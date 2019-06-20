2153dba6192d6e7698542ad5ddd8e6b5158d032f
Shu <shu.liang89@gmail.com>
2019-06-20 15:32:34 -0400

    home page now works

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,97 +1,150 @@
 //home.js
 
-import React from 'react';
+import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import "../styles/home.css";
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+import {fetchAllPostsThunk, addNewPostThunk } from '../actions/actionPost';
 import like from '../imgs/like.png';
 import dislike from '../imgs/dislike.png'
-import '../styles/home.css';
-import Input from './createPostForm';
+
+class Home extends Component{
+  constructor(props){
+      super(props);
+      this.state={
+          username: '',
+          imageUrl: '',
+          createPost: false,
+          sideBar: false
+      }
+  }
+
+  componentDidMount() {
+    console.log("in mount ");
+    this.props.getAllPost();
+  }
+
+  renderForm = () =>{
+      this.setState({createPost: true});
+  }
+  closeForm = () => {
+      this.setState({createPost: false});
+  }
+  showSidebar = () => {
+    console.log("---onMouseEnter----", this.state.sideBar);
+        this.setState({sideBar: true})
+  }
+
+  hideSideBar = () =>{
+    console.log("---onMouseLeave----", this.state.sideBar);
+      this.setState({sideBar: false});
+  }
+
+  TempForm = (
+      <div  className="three_form" >
+      <form  onSubmit={this.closeForm} className="modal-main" >
+          <label>Title</label>
+          <br />
+          <input type="text" name="title" />
+          <br/>
+          <label>Body</label>
+          <br />
+          <input type="text" name="body" id="three_body" />
+          <br/>
+          <input type="submit" value="submit"/>
+      </form>
+      </div>
+  )
+
+  sideBarView = (
+      <div className="three_sidebar" onMouseLeave={this.hideSideBar} >
+      <div className="three_credentials three_sideSub">
+          <img src="https://data.whicdn.com/images/320568210/large.jpg" alt="Cartoon Girl"/>
+          <h3> Cartoon Girl</h3>
+      </div>
+      <div className="three_createPostBtn three_sideSub">
+              <button id="three_createPost" onClick={this.renderForm}>Create Post</button>
+      </div>
+  </div>
+  )
 
 
-class Home extends React.Component{
-    constructor(props){
-        super(props);
-        this.state={
-            username: '',
-            imageUrl: '',
-            createPost: false,
-            sideBar: false
-        }
-    }
+  render(){
+    const showHide = this.state.createPost ? "three_form display-block" : "three_form  display-none";
 
-    renderForm = () =>{
-        this.setState({createPost: true});
-    }
-    closeForm = () => {
-        this.setState({createPost: false});
-    }
-    showSidebar = () => {
-          this.setState({sideBar: true})
-    }
+    if(this.props.postReducer.length === 0){
+      return(
+          <div className="three_wrapper">
 
-    hideSideBar = () =>{
-        this.setState({sideBar: false});
-    }
+            <div className="three_main">
+              <header>
+                <h1>Welcome</h1>
+              </header>
 
-    sideBarView = (
-        <div className="three_sidebar" onMouseLeave={this.hideSideBar} >
-        <div className="three_credentials three_sideSub">
-            <img src="https://data.whicdn.com/images/320568210/large.jpg" alt="Cartoon Girl"/>
-            <h3> Cartoon Girl</h3>
-        </div>
-        <div className="three_createPostBtn three_sideSub">
-                <button id="three_createPost" onClick={this.renderForm}>Create Post</button>
-        </div>
-    </div>
-    )
-
-    TempForm = (
-        <div  className="three_form" >
-        <form  onSubmit={this.closeForm} className="modal-main" >
-            <label>Title</label>
-            <br />
-            <input type="text" name="title" />
-            <br/>
-            <label>Body</label>
-            <br />
-            <input type="text" name="body" id="three_body" />
-            <br/>
-            <input type="submit" value="submit"/>
-        </form>
-        </div>
-    )
-
-
-    render(){
-        const showHide = this.state.createPost ? "three_form display-block" : "three_form  display-none";
-        const questions = ["Why is the sky blue ?", "Can we teach dogs to talk ?", "How many jelly beans can fit into the pacific ocean ?", "What is the best Island to vacation on ?", " Who was the greatest man alive ?"];
-        const postList = questions.map(post => {
-            return(
-            <div className="three_individual">
-                {post}
-                <p><img src={like} alt="like" style={ {height: 30}}/>{" "}
-                <img src={dislike} alt="dislike" style={ {height: 30}}/></p>
+              {this.state.createPost ? <div className={showHide} >{this.TempForm}</div>: <div></div>}
+              <div className="three_list">
+                <p> Recent: </p>
+                <p>There are no posts</p>
+              </div>
             </div>
-        )});
-        return(
-            <div className="three_wrapper" >
-                <div className="three_main">
-                    <header>
-                        <h1>Welcome</h1>
-                        <Link to="/signout"><button>sign out</button></Link>
-                    </header>
-                    {this.state.createPost ? <div className={showHide} >{this.TempForm}</div>: <div></div>}
-                    {/*<CreatePost show={this.state.createPost} handleClose={this.closeForm} /> */}
-                    <div className="three_list">
-                        <h3>Recent: </h3>
-                        {postList}
+
+              {this.state.sideBar ? this.sideBarView : <div  id="three_default" onMouseEnter={this.showSidebar}> </div> }
+
+          </div>
+      );
+    } else {
+      return(
+          <div className="three_wrapper">
+            <div className="three_main">
+              <header>
+                <h1>Welcome</h1>
+              </header>
+
+              {this.state.createPost ? <div className={showHide} >{this.TempForm}</div>: <div></div>}
+              <div className="three_list">
+                <p> Recent: </p>
+                {this.props.postReducer.map((p) => {
+                  return(
+                    <div className="three_individual">
+                        <h3>{p.title}</h3>
+                        <p><img src={like} alt="like" style={ {height: 30}}/>{" "}
+                        <img src={dislike} alt="dislike" style={ {height: 30}}/></p>
                     </div>
-                </div>
-                {this.state.sideBar ? this.sideBarView : <div  id="three_default" onMouseEnter={this.showSidebar}> </div> }
-
+                  )
+                })}
+              </div>
             </div>
-        )
+
+
+              {this.state.sideBar ? this.sideBarView : <div  id="three_default" onMouseEnter={this.showSidebar}> </div> }
+
+          </div>
+      );
     }
+
+
+  }
 }
 
-export default Home;
+function mapStateToProps(state){
+  return {
+    postReducer: state.post
+  };
+}
+
+// pass functions as to props in order to call props.addNewCampus
+// function matchDispatchToProps(dispatch){
+//   return bindActionCreators({ addNewCampus: fetchCampusThunk}, dispatch);
+// }
+
+
+// Map dispatch to props;
+function matchDispatchToProps(dispatch) {
+  return {
+    addNewPost: (post, userId) => dispatch(addNewPostThunk(post, userId)),
+    getAllPost: () => dispatch(fetchAllPostsThunk())
+  }
+}
+
+export default connect(mapStateToProps, matchDispatchToProps)(Home);
